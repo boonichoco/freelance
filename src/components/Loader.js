e64e48f80d6c2531548e95b88a81f7ae7c68d38f
Thomas Faucher <thomasfaucher9@gmail.com>
2022-09-21 19:11:55 +0200

    loader page

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Loader = () => {
     return (
         <div className='LoaderContent'>
-            
+            <svg width="100vw" height="20">
+                <rect width="100%" height="100%" fill="#46237a" />
+            </svg>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+//import App from './App';
+import Loader from './components/Loader';
 import './styles/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Loader />
   </React.StrictMode>,
   document.getElementById("root")
 );
